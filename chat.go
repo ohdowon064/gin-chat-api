315bbb7eea8c0801b58b60814cbd0a9915f1eda0
Dowon Oh <dhehdnjs03@gmail.com>
2023-03-27 23:34:07 +0900

    join, leave chat room

--- chat.go
+++ chat.go
@@ -7,7 +7,14 @@ type ChatRoom struct {
 	Name string `json:"name"`
 }
 
+type User struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
 var ChatRooms []ChatRoom
+var ChatRoomToUser map[ChatRoom]User
+var UserToChatRoom map[User]ChatRoom
 
 func CreateChatRoom(name string) ChatRoom {
 	id := uuid.New().String()
@@ -17,4 +24,15 @@ func CreateChatRoom(name string) ChatRoom {
 	}
 	ChatRooms = append(ChatRooms, chatRoom)
 	return chatRoom
+}
+
+func JoinChatRoom(user User, chatRoom ChatRoom) {
+	ChatRoomToUser[chatRoom] = user
+	UserToChatRoom[user] = chatRoom
+}
+
+func LeaveChatRoom(user User) {
+	chatRoom := UserToChatRoom[user]
+	delete(ChatRoomToUser, chatRoom)
+	delete(UserToChatRoom, user)
 }
